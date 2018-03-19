0f6aa699ab046465c7393ea49971605956dd8543
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-03-18 20:18:58 -0500

    added pseudocode for updating devoured status

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,14 +1,24 @@
-console.log("We are linked");
+console.log("We are linked")
 
-// $(document).ready(function(){
+$(document).ready(function() {
+  //  on click events for #text-enter button aka submit
+  $("#text-enter").on("click", function(e) {
+    e.preventDefault()
+    var newBurger = {
+      burger_name: $("#enter_text").val()
+    }
+    console.log(newBurger.burger_name)
+  })
 
-// //  on click events for #text-enter button aka submit 
-// $("#text-enter").on("click", function(e){
-//     e.preventDefault()
-//     var newBurger = {
-//         burger_name: $("#enter_text").val()
-//     }
-//     console.log(newBurger);
-// })
-
-// })
+  $("#devour-btn").on("click", function() {
+    updateDevoured()
+  })
+//  pseudo code for updating devoured status 
+  function updateDevoured(Burger) {
+    $.ajax({
+      method: "UPDATE",
+      url: "/burgers/update/:id " + id,
+      data: Burger
+    })
+  }
+})
